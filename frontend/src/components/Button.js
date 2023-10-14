18c7bffaeb7463d625fbae2f5a19dc013fb9f287
elddc <emmachen2010@gmail.com>
2023-10-14 17:17:58 -0500

    Add Flask server workshop code

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,15 +1,22 @@
 import {useState, useEffect} from "react";
 import "./button.css";
+import axios from "axios";
 
 const Button = ({text}) => {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        // alert("do API calls here");
-        setCount(12);
+        // setCount(12);
+        axios.get("http://127.0.0.1:5000/bar").then(res => console.log(res))
     }, []);
 
-    return <div className="button" onClick={() => setCount(count + 1)}>{text}: {count}</div>
+    const foo = () => {
+        axios.post("http://127.0.0.1:5000/foo", {"a": "b"})
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    }
+
+    return <div className="button" onClick={foo}>{text}: {count}</div>
 }
 
 export default Button;
--- server/app.py
+++ server/app.py
@@ -1,3 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+@app.post("/foo")
+def foo():
+    print(request.data)
+    return "yay"
+
+@app.get("/bar")
+def bar():
+    return "hello world"
