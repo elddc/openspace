56c60e7de81c90b6597e6c5f673d39e5294c48f1
ddrewi <84168601+ddrewi@users.noreply.github.com>
2023-10-21 17:41:44 -0500

    Initial post setup

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,36 @@ CORS(app)
 @app.post("/foo")
 def foo():
     # post requests can access data with request.json[key]
+    # __tablename__
+    # id
+    # name
+    # address
+    # location
+    # capacity
+    # busyness
+    # rooms
+    # inputs
+    # last_updated
+
+    # building_id
+    # room_number
+    # busyness
+
+
+    # with Session(engine) as session:
+    user = model.Building(
+        # id="",
+        name=str(request.json["name"]),
+        address=str(request.json["address"]),
+        location=str(request.json["location"]),
+        capacity=str(request.json["capacity"]),
+        busyness=SmallInteger(request.json["busyness"]),
+        
+        # last_updated="",
+    )
+    db.session.add(user)
+
+    db.session.flush()
     return str(request.json["busyness"])
 
 @app.get("/bar")
