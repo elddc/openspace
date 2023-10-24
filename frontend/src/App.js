596babe5f9c38522cf1495a4318d6f00f6af28a9
elddc <emmachen2010@gmail.com>
2023-10-24 13:06:49 -0500

    Move application logic to Form component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 
-import Button from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>CIF</p>
-                <div className="row">
-                    <Button text={"Update building busyness"}/>
-                    <Button text={"Update building busyness"}/>
-                </div>
+                <Form />
             </header>
         </div>
     );
--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -2,43 +2,8 @@ import {useState, useEffect} from "react";
 import "./button.css";
 import axios from "axios";
 
-const Button = ({text}) => {
-    const [busyness, setBusyness] = useState(false);
-    const [inProgress, setInProgress] = useState(true)
-
-    // initial get request
-    useEffect(() => {
-        setInProgress(true);
-        axios.get("http://127.0.0.1:5000/building", {
-            params: { "name": "CIF" }
-        }).then(res => {
-            console.log(res)
-            setBusyness(res.data);
-            setInProgress(false);
-        }).catch(err => console.log(err));
-    }, []);
-
-    // update db every time busyness changes
-    useEffect(() => {
-        if (busyness) {
-            axios.post("http://127.0.0.1:5000/update", {
-                name: "CIF",
-                busyness
-            }).then(res => {
-                console.log(res);
-            }).catch(err => console.log(err));
-        }
-        else {
-            console.log("GET request in progress, please wait for the page to finish loading");
-        }
-    }, [busyness]);
-
-    // increment busyness
-    const updateBusyness = () => {
-        setBusyness(busyness % 5 + 1);
-    }
-
-    return <div className="button" onClick={updateBusyness}>{text}: {busyness}</div>
+const Button = ({text, callback}) => {
+    return <div className="button" onClick={callback}>{text}</div>
 }
 
 export default Button;
