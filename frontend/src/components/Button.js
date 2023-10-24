93fef6cd75f2fbb6ba7ba148fa886744815f92e4
elddc <emmachen2010@gmail.com>
2023-10-24 13:17:01 -0500

    Remove excess imports

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,6 +1,5 @@
 import {useState, useEffect} from "react";
 import "./button.css";
-import axios from "axios";
 
 const Button = ({callback, children}) => {
     return <div className="button" onClick={callback}>{children}</div>
