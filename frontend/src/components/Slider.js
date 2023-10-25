df9d3ab05ce4133eca7bf290cad9076ff16c3baf
sbaek21 <jasmine4081@gmail.com>
2023-10-24 19:38:27 -0500

    Update slider appearance

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -1,6 +1,7 @@
+import "./slider.css";
 const Slider = ({value, setValue}) => {
     return <div>
-        <input type="range" id="busyness" name="volume" min="0" max="5"
+        <input className="slider" type="range" id="busyness" name="volume" min="0" max="5"
                value={value} onChange={(ev) => {setValue(ev.target.value)}}
         />
     </div>
