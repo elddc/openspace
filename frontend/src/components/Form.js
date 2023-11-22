b8ea499238d439e0d0490ce6c199ab60eaa1bbdb
elddc <emmachen2010@gmail.com>
2023-11-22 00:48:04 -0600

    Move app logic to App.js, move dropdown to map area

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -8,65 +8,12 @@ import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import "react-circular-progressbar/dist/styles.css";
 import "./form.css";
 
-const Form = ({text}) => {
-    const [busyness, setBusyness] = useState(-1);
-    const [progress, setProgress] = useState(-1);
-    const [currentBuilding, setCurrentBuilding] = useState(null);
-    const [buildings, setBuildings] = useState(null);
+const Form = ({currentBuilding, progress, busyness, setBusyness}) => {
     const debounceUpdate = useRef();
 
-    // initial get request, gets all building data
     useEffect(() => {
-        const controller = new AbortController();
-        axios.get("http://127.0.0.1:5000/building", {
-            signal: controller.signal
-        }).then(res => {
-            console.log("buildings: " , res.data);
-            console.log("names: " + res.data.map(({name}) => name));
-            setBuildings(res.data);
-            setCurrentBuilding("CIF"); // default
-            debounceUpdate.current = debounce(updateBusyness, 10);
-        }).catch(err => console.log(err));
-        return () => {controller.abort()};
-    }, []);
-
-    // get busyness for a single building
-    useEffect(() => {
-        const controller = new AbortController();
-        if (currentBuilding) {
-            axios.get("http://127.0.0.1:5000/building", {
-                params: { "name": currentBuilding },
-                signal: controller.signal
-            }).then(res => {
-                // console.log("busyness: " + res.data);
-                setBusyness(res.data);
-                setProgress(res.data);
-            }).catch(err => console.log(err));
-        }
-        else {
-            console.log("GET request in progress, please wait for the page to finish loading");
-        }
-        return () => {controller.abort()};
-    }, [currentBuilding]);
-
-    // update db every time busyness changes
-    useEffect(() => {
-        if (busyness >= 0) {
-            axios.post("http://127.0.0.1:5000/building", {
-                name: currentBuilding,
-                busyness
-            }).then(res => {
-                console.log("progress: " + res.data);
-                setProgress(res.data);
-            }).catch(err => {
-                console.log(err);
-                console.log("cancelled " + busyness);
-            });
-        }
-        else {
-            console.log("GET request in progress, please wait for the page to finish loading");
-        }
-    }, [busyness]);
+        debounceUpdate.current = debounce(updateBusyness, 10);
+    }, [setBusyness]);
 
     // debounced update function to prevent overwrites
     const updateBusyness = (val) => {
@@ -74,22 +21,12 @@ const Form = ({text}) => {
     }
 
     return <div className="form center">
-        {buildings &&
-            <div className="dropdown">
-                <Select
-                    defaultValue={currentBuilding}
-                    onChange={({value}) => setCurrentBuilding(value)}
-                    options={buildings.map(({name}) => ({value: name, label: name}))}
-                />
-            </div>
-        }
         <div className="box center mb-row">
             <h1>{currentBuilding || "Loading..."}</h1>
             <div className="progress-container">
                 {progress < 0 ||
                     <CircularProgressbarWithChildren
                         value={progress * 20}
-                        // text={progress * 20 + "%"}
                         strokeWidth={12}
                     >
                         <div className="progress-percent">{progress * 20}%</div>
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,7 +1,23 @@
+import Select from "react-select";
 import "./map.css";
 
-const Map = () => {
-	return <div className="map center">Todo :D</div>;
+const Map = ({buildings, currentBuilding, setCurrentBuilding}) => {
+	console.log(currentBuilding)
+
+	return <div className="map center">
+		{buildings &&
+			<div className="dropdown">
+				<Select
+					value={{value: currentBuilding, label: currentBuilding}}
+					onChange={({value}) => setCurrentBuilding(value)}
+					options={buildings.map(({name}) => ({value: name, label: name}))}
+					autoFocus
+					isSearchable
+				/>
+			</div>
+		}
+		Todo :D
+	</div>;
 };
 
 export default Map;
