ed57e99248df7ec3cf785fd4a603f071b4491d16
Rachel <78095303+rachelm27@users.noreply.github.com>
2023-11-27 21:51:12 -0600

    preliminary map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,7 +1,33 @@
 import "./map.css";
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+//var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+ 
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3BlbnNwYWNldWl1YyIsImEiOiJjbHBoY2lxbXUwNWxwMmtxcWtwbm00ZHd3In0.53dUgjuNw_jH0aN0lkjlCQ';
 
 const Map = () => {
-	return <div className="map center">Todo :D</div>;
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-88.2073);
+	const [lat, setLat] = useState(40.1106);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		if (map.current) return; // initialize map only once
+		map.current = new mapboxgl.Map({
+		  container: mapContainer.current,
+		  style: 'mapbox://styles/mapbox/streets-v12',
+		  center: [lng, lat],
+		  zoom: zoom
+		});
+	  });
+
+ 	return <div>
+		<div className="map center" style={{width: 800}}>Map</div>
+		<div ref={mapContainer} className="map-container" />
+	</div>
+  
 };
 
 export default Map;
