79f0944b4e4082422c1bf05b46bee86866a8374a
patatoes1235 <kzhou12356@gmail.com>
2023-11-29 02:09:50 -0600

    push to db

--- server/scrape.py
+++ server/scrape.py
@@ -19,24 +19,24 @@ Session = sessionmaker(engine)
 base = "https://uiuc.libcal.com/"
 all = "allspaces" # base + all
 id  = { # base + "spaces/?lid=" + id of desired library
-    "chemistry": 5903,
-    "funk": 3604,
-    "grainger": 3606,
-    "international": 5766,
-    "main": 3608,
-    "music": 3153,
-    "studio": 16231
+    "Chemistry Library": 5903, 
+    "Funk ACES Library": 3604, 
+    "Grainger Library": 3606,
+    "Main Library": 3608,
+    "Music & Performing Arts Library": 3153,
+    "studio": 16231,
+    # "international": 5766
 }
+
 args = "&gid=0&c=0" # additional arguments
 
 busyness = {
-    "chemistry": 0,
-    "funk": 0,
-    "grainger": 0,
-    "international": 0,
-    "main": 0,
-    "music": 0,
-    "studio": 0
+    "Chemistry Library": [0, 0],
+    "Funk ACES Library": [0, 0],
+    "Grainger Library": [0, 0],
+    "Main Library": [0, 0],
+    "Music & Performing Arts Library": [0, 0],
+    "studio": [0, 0]
 }
 
 rounded = datetime.now() - (datetime.now() - datetime.min) % timedelta(minutes=30)
@@ -61,36 +61,35 @@ for key in id:
     driver.get(url)
 
     # wait for page load
-    sleep(1)
+    sleep(0.5)
 
     # # extract data
     events = [ev.get_attribute("title") for ev in driver.find_elements(By.CLASS_NAME, "fc-event-today")
               if current_time in ("0" + ev.get_attribute("title")) or next_time in ("0" + ev.get_attribute("title"))]
 
-    if len(events) == 0:
-        busyness[key] = 0
-    else:
-        total_unavailable = 0
+    total_unavailable = 0
 
-        for ev in events:
-            print(ev)
-            if "Unavailable" in ev:
-                total_unavailable = total_unavailable + 1
+    for ev in events:
+        print(ev)
+        if "Unavailable" in ev:
+            total_unavailable = total_unavailable + 1
 
-        if total_unavailable == 0:
-            busyness[key] = 1
-        else:
-            print("Total: " + str(len(events)))
-            print("Unavailable: " + str(total_unavailable))
-            ratio = float(total_unavailable)/len(events) * 100
+    print("Total: " + str(len(events)))
+    print("Unavailable: " + str(total_unavailable))
 
-            busyness[key] = round(ratio)
+    busyness[key] = [total_unavailable, len(events)]
 
-        print("Busyness: " + str(busyness[key]))
+    print("Busyness: " + str(busyness[key]))
 
 # quit driver
 driver.quit()
 
-with Session.begin() as session:
+print(busyness)
+busyness["Main Library"] = [busyness["Main Library"][i] + busyness["studio"][i] for i in range(2)]
+busyness.pop("studio")
+print(busyness)
+
+with Session() as session:
     for building in busyness:
-        update_building(session, building, busyness[building])
+        ratio = busyness[building]
+        update_building(session, building, int(float(ratio[0])/ratio[1] * 100) if ratio[1] != 0 else 0)
